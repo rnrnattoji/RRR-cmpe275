2360fee56611ccf6455072b6ba796d5f5a10b68e
Rohit Vardam <46842159+varoOP@users.noreply.github.com>
2024-02-19 20:38:40 -0800

    Read message of any length

--- Mini-Project-1/socket-3code/java-src/socket/src/gash/socket/BasicClient.java
+++ Mini-Project-1/socket-3code/java-src/socket/src/gash/socket/BasicClient.java
@@ -3,7 +3,7 @@ package gash.socket;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.OutputStream;
+// import java.io.OutputStream;
 import java.net.Socket;
 
 import gash.payload.BasicBuilder;
--- Mini-Project-1/socket-3code/java-src/socket/src/gash/socket/SessionHandler.java
+++ Mini-Project-1/socket-3code/java-src/socket/src/gash/socket/SessionHandler.java
@@ -1,7 +1,8 @@
 package gash.socket;
 
 import java.io.BufferedInputStream;
-import java.io.InterruptedIOException;
+import java.io.ByteArrayOutputStream;
+// import java.io.InterruptedIOException;
 import java.net.Socket;
 
 import gash.payload.BasicBuilder;
@@ -55,35 +56,55 @@ class SessionHandler extends Thread {
 		System.out.println("Session " + sessionId + " started\n");
 
 		try {
-			connection.setSoTimeout(2000);
+			connection.setSoTimeout(0);
 			var in = new BufferedInputStream(connection.getInputStream());
 
 			if (in == null)
 				throw new RuntimeException("Unable to get input streams");
 
-			byte[] raw = new byte[2048];
 			BasicBuilder builder = new BasicBuilder();
 			while (forever) {
-				try {
-					int len = in.read(raw);
-					if (len == 0 )
-						continue;
-					else if (raw[0] == 0) {
-						continue;
-					} else if(len == -1) {
+				ByteArrayOutputStream buf = new ByteArrayOutputStream();
+				int nextByte;
+
+				while ((nextByte = in.read())!= -1) {
+					if (nextByte == 0) {
 						break;
 					}
+					buf.write(nextByte);
+				}
 
-					Message msg = builder.decode(raw);
-					if (msg != null) {
-						String modifiedName = msg.getName() + " (Session " + sessionId + ")";
-						String modifiedMsg = "from " + modifiedName + ", to group: " + msg.getGroup() + ", text: " + msg.getText();
-						System.out.println(modifiedMsg + "\n");
-					}
+				if(nextByte == -1) {
+					stopSession();
+					break;
+				}
+
+				if (buf.size() == 0) {
+					continue;
+				}
+
+				byte[] messageBytes = buf.toByteArray();
+
+			
+				// byte[] raw = new byte[2048];
+				// try {
+				// 	int len = in.read(raw);
+				// 	if (len == 0 )
+				// 		continue;
+				// 	else if (raw[0] == 0) {
+				// 		continue;
+				// 	} else if(len == -1) {
+				// 		break;
+				// 	}
+
+				Message msg = builder.decode(messageBytes);
+				if (msg != null) {
+					String modifiedName = msg.getName() + " (Session " + sessionId + ")";
+					String modifiedMsg = "from " + modifiedName + ", to group: " + msg.getGroup() + ", text: " + msg.getText();
+					System.out.println(modifiedMsg + "\n");
+				}
 					// System.out.println(msg + "\n");
 					
-				} catch (InterruptedIOException ioe) {
-				}
 			}
 		} catch (Exception e) {
 			e.printStackTrace();
