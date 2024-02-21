f9437ae0d261d9981f29e8c3757c24b42b81a7ac
Rohit Vardam <46842159+varoOP@users.noreply.github.com>
2024-02-20 22:06:58 -0800

    fix: handle issue null character in between a message

--- Mini-Project-1/socket-3code/java-src/socket/src/gash/socket/SessionHandler.java
+++ Mini-Project-1/socket-3code/java-src/socket/src/gash/socket/SessionHandler.java
@@ -1,122 +1,125 @@
 package gash.socket;
 
+import gash.payload.BasicBuilder;
+import gash.payload.Message;
 import java.io.BufferedInputStream;
 import java.io.ByteArrayOutputStream;
 // import java.io.InterruptedIOException;
 import java.net.Socket;
-
-import gash.payload.BasicBuilder;
-import gash.payload.Message;
-
+import java.util.Arrays;
 
 /**
- * 
+ *
  * @author gash
- * 
+ *
  */
 class SessionHandler extends Thread {
-	private static int sessionCount = 0;
-	private final int sessionId;
-	private Socket connection;
-	// private String name;
-	private boolean forever = true;
-	
 
-	public SessionHandler(Socket connection) {
-		this.connection = connection;
-		
-		// allow server to exit if
-		this.setDaemon(true);
+  private static int sessionCount = 0;
+  private final int sessionId;
+  private Socket connection;
+  // private String name;
+  private boolean forever = true;
 
-		synchronized (SessionHandler.class) {
-            sessionCount++; 
-            sessionId = sessionCount;
+  public SessionHandler(Socket connection) {
+    this.connection = connection;
+
+    // allow server to exit if
+    this.setDaemon(true);
+
+    synchronized (SessionHandler.class) {
+      sessionCount++;
+      sessionId = sessionCount;
+    }
+  }
+
+  /**
+   * stops session on next timeout cycle
+   */
+  public void stopSession() {
+    forever = false;
+    if (connection != null) {
+      try {
+        connection.close();
+      } catch (Exception e) {
+        e.printStackTrace();
+      }
+    }
+    connection = null;
+  }
+
+  /**
+   * process incoming data
+   */
+  public void run() {
+    System.out.println("Session " + sessionId + " started\n");
+    try {
+      connection.setSoTimeout(0);
+      BufferedInputStream in = new BufferedInputStream(
+        connection.getInputStream()
+      );
+
+      while (forever) {
+        ByteArrayOutputStream buf = new ByteArrayOutputStream();
+        boolean messageComplete = false;
+
+        while (!messageComplete) {
+          byte[] dataChunk = new byte[1024]; 
+          int bytesRead = in.read(dataChunk, 0, dataChunk.length); 
+
+          if (bytesRead == -1) { 
+            forever = false;
+            break;
+          }
+
+          boolean isNullChunk = true;
+          for (int i = 0; i < bytesRead; i++) {
+            if (dataChunk[i] != 0) {
+              isNullChunk = false;
+              break;
+            }
+          }
+
+          if (isNullChunk) {
+            continue; 
+          }
+
+          if (bytesRead > 0 && dataChunk[bytesRead - 1] == 0) {
+            messageComplete = true; 
+            bytesRead--;
+          }
+
+          buf.write(dataChunk, 0, bytesRead);
         }
-	}
 
-	/**
-	 * stops session on next timeout cycle
-	 */
-	public void stopSession() {
-		forever = false;
-		if (connection != null) {
-			try {
-				connection.close();
-			} catch (Exception e) {
-				e.printStackTrace();
-			}
-		}
-		connection = null;
-	}
-
-	/**
-	 * process incoming data
-	 */
-	public void run() {
-		System.out.println("Session " + sessionId + " started\n");
-
-		try {
-			connection.setSoTimeout(0);
-			var in = new BufferedInputStream(connection.getInputStream());
-
-			if (in == null)
-				throw new RuntimeException("Unable to get input streams");
-
-			BasicBuilder builder = new BasicBuilder();
-			while (forever) {
-				ByteArrayOutputStream buf = new ByteArrayOutputStream();
-				int nextByte;
-
-				while ((nextByte = in.read())!= -1) {
-					if (nextByte == 0) {
-						break;
-					}
-					buf.write(nextByte);
-				}
-
-				if(nextByte == -1) {
-					stopSession();
-					break;
-				}
-
-				if (buf.size() == 0) {
-					continue;
-				}
-
-				byte[] messageBytes = buf.toByteArray();
-
-			
-				// byte[] raw = new byte[2048];
-				// try {
-				// 	int len = in.read(raw);
-				// 	if (len == 0 )
-				// 		continue;
-				// 	else if (raw[0] == 0) {
-				// 		continue;
-				// 	} else if(len == -1) {
-				// 		break;
-				// 	}
-
-				Message msg = builder.decode(messageBytes);
-				if (msg != null) {
-					String modifiedName = msg.getName() + " (Session " + sessionId + ")";
-					String modifiedMsg = "from " + modifiedName + ", to group: " + msg.getGroup() + ", text: " + msg.getText();
-					System.out.println(modifiedMsg + "\n");
-				}
-					// System.out.println(msg + "\n");
-					
-			}
-		} catch (Exception e) {
-			e.printStackTrace();
-		} finally {
-			try {
-				System.out.println("Session " + sessionId + " ending");
-				System.out.flush();
-				stopSession();
-			} catch (Exception re) {
-				re.printStackTrace();
-			}
-		}
-	}
-
+        byte[] messageBytes = buf.toByteArray();
+        if (messageBytes.length > 0) {
+          BasicBuilder builder = new BasicBuilder();
+          Message msg = builder.decode(messageBytes);
+          if (msg != null) {
+            String modifiedName =
+              msg.getName() + " (Session " + sessionId + ")";
+            String modifiedMsg =
+              "from " +
+              modifiedName +
+              ", to group: " +
+              msg.getGroup() +
+              ", text: " +
+              msg.getText();
+            System.out.println(modifiedMsg + "\n");
+          }
+        }
+      }
+    } catch (Exception e) {
+      e.printStackTrace();
+    } finally {
+      try {
+        System.out.println("Session " + sessionId + " ending");
+        System.out.flush();
+        stopSession();
+      } catch (Exception re) {
+        re.printStackTrace();
+      }
+    }
+  }
 } // class SessionHandler
