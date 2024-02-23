c976b5ebf33b24e3aa240e2d1cc0aface0f59527
varoOP <46842159+varoOP@users.noreply.github.com>
2024-02-22 20:02:14 -0800

    Update test.py and fix format error in python

--- Mini-Project-1/socket-3code/test.py
+++ Mini-Project-1/socket-3code/test.py
@@ -3,8 +3,7 @@ import random
 import string
 import time
 
-def generate_message():
-    message_length = 6000
+def generate_message(message_length):
     message_content = ''.join(random.choices(string.ascii_letters + string.digits, k=message_length))
     message = f"{message_content}\0"
     total_length = len(message) + 4 + 6 + 11
@@ -15,20 +14,17 @@ def generate_message():
 def main():
     host = input("Enter the host address: ")
     port = int(input("Enter the port number: "))
+    message_length = int(input("Enter the message length: "))
+    num_messages = int(input("Enter the number of messages to be sent: "))
 
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
         sock.connect((host, port))
 
-        num_messages = 1000
-        max_length = 0 
-
         for _ in range(num_messages):
-            msg = generate_message()
-            max_length = max(max_length, len(msg))
+            msg = generate_message(message_length)
             sock.sendall(msg.encode('utf-8'))
-            # print(f"Sent: {msg[:50]}...") 
         
-        # print(f"Total messages sent: {num_messages}")
+        print(f"Total messages sent: {num_messages}")
 
 if __name__ == "__main__":
     main()
